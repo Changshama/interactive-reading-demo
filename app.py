95c5d93e3de614ddd0be39aa4b9e78d6cd0b77cc
Changshama <machangsha@gmail.com>
2022-03-10 14:35:42 +0000

    app with dynamic db update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import functools
 import wave
-from flask import Flask, views, request, flash, redirect, Response, render_template, Blueprint, current_app, session, url_for
+from flask import Flask, jsonify, views, request, flash, redirect, Response, render_template, Blueprint, current_app, session, url_for
 from sqlalchemy import Table
 from views import bp_nemo,login_required
 from flask_sqlalchemy import SQLAlchemy
@@ -37,6 +37,9 @@ def landing():
     que_answered = []
     if len(user.que_answered) > 0:
       que_answered = list(map(int, user.que_answered.split(';')))
+      print('answered questions include the following:')
+      print(que_answered)
+      print('------------------------------------------')
     question = db.session.query(Question).filter(~Question.id.in_(que_answered))
 
     # create question for all pages
@@ -46,20 +49,47 @@ def landing():
         page_conf[page-1]['que_audio'] = page_que.audio
         page_conf[page-1]['ans_audio'] = 'static/audio/100/'+page_que.ans_audio
         page_conf[page-1]['ans_keys'] = page_que.ans_keys.split(';')
+        page_conf[page-1]['que_id'] = page_que.id
+
       else:
+        page_conf[page-1]['que_audio'] = ''
+        page_conf[page-1]['ans_audio'] = ''        
         page_conf[page-1]['ans_keys'] = []
+        page_conf[page-1]['que_id'] = 0
 
     return render_template('100_floor/landing.html', 
                             username =session["username"],
                             question_name = 'landing-question.wav',
                             next_page = 1,
+                            que_id = 0,
                             ans_path = "static/audio/start-reading.wav",
                             keys=['好了','准备'])
+
+@bp_100.route('/process_answer', methods=['POST', 'GET'])
+def process_answer():
+  if request.method == "POST":
+    ans_data = request.get_json()
+    user = db.session.query(User).filter_by(username = session["username"]).first()
+    que_answered_before = user.que_answered
+
+    print(ans_data[0]['question'],ans_data[1]['result'])
+
+    # update que answered for the session user
+    if ans_data[0]['question'] > 0 and ans_data[1]['result']:
+      if que_answered_before:
+        user.que_answered += ';'+ str(ans_data[0]['question'])
+      else:
+        user.que_answered += str(ans_data[0]['question'])
+      db.session.commit()
+      print('db updated!')
+    
+    return 'ok'
+
 
 @bp_100.route('/<int:page>')
 @login_required
 def innerbook(page):
-    idx = page -1    
+    idx = page - 1    
     return render_template('100_floor/innerbook.html', 
                         username =session["username"],
                         img_name = page_conf[idx]['img_name'],
@@ -67,7 +97,8 @@ def innerbook(page):
                         question_name = page_conf[idx]['que_audio'],
                         next_page=page_conf[idx]['next_page'],
                         ans_path = page_conf[idx]['ans_audio'],  
-                        keys=page_conf[idx]['ans_keys'])
+                        keys=page_conf[idx]['ans_keys'],
+                        que_id = page_conf[idx]['que_id'])
 
 nemo_1_keys = ['Orange', 'Stripe', 'Black', 'White', 'orange', 'white']
 nemo_2_keys = ['Forget', 'Remember', 'Memory', 'loss', 'Issue', 'forget', 'remember']
