b63d0d2c9c29ee3ceff4fe4b1ea297b94d70369c
Changshama <machangsha@gmail.com>
2022-03-29 22:28:34 +0000

    new data

--- conf.py
+++ conf.py
@@ -71,3 +71,17 @@ page_conf_pigeon =[{"id": 'page1', "img_name": 'p1.png', "audio_name": 'p1.wav',
             {"id": 'page16', "img_name": 'p16.png', "audio_name": 'p16.wav', "next_page":17, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},   
             {"id": 'page17', "img_name": 'p17.png', "audio_name": 'p17.wav', "next_page":1, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
 ]
+page_conf_worm =[{"id": 'page1', "img_name": 'p1.png', "audio_name": 'p1.wav', "next_page":2, 'que_id':'', 'que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+            {"id": 'page2', "img_name": 'p2.png', "audio_name": 'p2.wav', "next_page":3, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+            {"id": 'page3', "img_name": 'p3.png', "audio_name": 'p3.wav', "next_page":4, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+            {"id": 'page4', "img_name": 'p4.png', "audio_name": 'p4.wav', "next_page":5, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+            {"id": 'page5', "img_name": 'p5.png', "audio_name": 'p5.wav', "next_page":6, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+             {"id": 'page6', "img_name": 'p6.png', "audio_name": 'p6.wav', "next_page":7, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+            {"id": 'page7', "img_name": 'p7.png', "audio_name": 'p7.wav', "next_page":8, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+            {"id": 'page8', "img_name": 'p8.png', "audio_name": 'p8.wav', "next_page":9, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},   
+            {"id": 'page9', "img_name": 'p9.png', "audio_name": 'p9.wav', "next_page":10, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+             {"id": 'page10', "img_name": 'p10.png', "audio_name": 'p10.wav', "next_page":11, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+            {"id": 'page11', "img_name": 'p11.png', "audio_name": 'p11.wav', "next_page":12, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},
+            {"id": 'page12', "img_name": 'p12.png', "audio_name": 'p12.wav', "next_page":13, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]},   
+            {"id": 'page13', "img_name": 'p13.png', "audio_name": 'p13.wav', "next_page":1, 'que_id':'','que_audio':'', 'ans_audio':'', 'count_max': 0, 'ans_keys':[]}
+]
