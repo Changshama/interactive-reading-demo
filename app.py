b50b1325b741944027da3c75732dedde5f6c9f44
Changshama <machangsha@gmail.com>
2022-03-30 15:34:09 +0000

    new bp

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import functools
 import wave
 from flask import Flask, jsonify, views, request, flash, redirect, Response, render_template, Blueprint, current_app, session, url_for
 from sqlalchemy import Table
-from views import login_required,page_config
+from utils import page_config, question_conf, login_required
+from utils import landing as land
+from utils import process_answer as process
+from utils import innerbook as inner
+from views import bp_r, bp_ic, bp_pipi
 from flask_sqlalchemy import SQLAlchemy
 import csv
 
@@ -20,45 +24,16 @@ class Question(db.Model):
     __table__ = Table('question', db.metadata,
                     autoload=True, autoload_with=db.engine)
 
-def question_conf(page_conf, uname, bookId, path_dir):
-    # filter out questions answered (correctly) by this user
-    user = db.session.query(User).filter_by(username = uname).first()
-    que_answered = []
-    if len(user.que_answered) > 0:
-      que_answered = list(map(int, user.que_answered.split(';')))
-      print('answered questions include the following:')
-      print(que_answered)
-      print('------------------------------------------')
-    question = db.session.query(Question).filter(~Question.id.in_(que_answered))
-
-    # create question for all pages
-    for page in range(1, len(page_conf)+1):
-      page_que = question.filter_by(book_id=bookId,level=user.level, page_id=page).first()
-      if page_que:
-        page_conf[page-1]['que_audio'] = page_que.audio
-        page_conf[page-1]['ans_audio'] = path_dir+page_que.ans_audio
-        page_conf[page-1]['ans_keys'] = page_que.ans_keys.split(';')
-        page_conf[page-1]['que_id'] = page_que.id
-        page_conf[page-1]['count_max'] = page_que.count_max
-
-      else:
-        page_conf[page-1]['que_audio'] = ''
-        page_conf[page-1]['ans_audio'] = ''        
-        page_conf[page-1]['ans_keys'] = []
-        page_conf[page-1]['que_id'] = 0
-
-    return page_conf
-
-bp_r = Blueprint('resources', __name__, static_folder='static',
+bp_green = Blueprint('green', __name__, static_folder='static',
                template_folder='templates')
 
-bp_ic = Blueprint('ic', __name__, static_folder='static',
+page_conf_worm = page_config('page_conf_worm.csv')
+
+bp_sally = Blueprint('sally', __name__, static_folder='static',
                template_folder='templates')
 
-bp_pipi = Blueprint('pipi', __name__, static_folder='static',
-               template_folder='templates')
 
-bp_100 = Blueprint('100_floor', __name__, static_folder='static',
+bp_100 = Blueprint('100', __name__, static_folder='static',
                template_folder='templates')
 
 page_conf = page_config('page_conf.csv')
@@ -82,299 +57,105 @@ bp_baba = Blueprint('baba', __name__, static_folder='static',
                template_folder='templates')
 
 page_conf_baba = page_config('page_conf_baba.csv')
-@bp_ic.route('/button')
-def button():
-  return render_template('ic/button.html')
-  
-@bp_ic.route('/dragon')
-def dragon():
-  return render_template('ic/dragon.html')
-
-@bp_ic.route('/danny')
-def danny():
-  return render_template('ic/danny.html')
-
-@bp_ic.route('/pigeon')
-def pigeon():
-  return render_template('ic/pigeon.html')
-
-@bp_pipi.route('/part1')
-def part1():
-  return render_template('pipi/part1.html')
-
-@bp_pipi.route('/part2')
-def part2():
-  return render_template('pipi/part2.html')
-
-@bp_r.route('/awards')
-def awards():
-  return render_template('resources/awards.html')
-@bp_r.route('/guide')
-def guidance():
-  return render_template('resources/guide.html')
-@bp_r.route('/authors')
-def authors():
-  return render_template('resources/authors.html')
-@bp_r.route('/illustrators')
-def illustrators():
-  return render_template('resources/illustrators.html')
 
 @bp_ft.route('/landing')
 @login_required
 def landing():
-    uname = session["username"]
-    question_conf(page_conf_firetruck, uname, 4, 'static/audio/firetruck/')
-
-    return render_template('firetruck/landing.html', 
-                            username =session["username"],
-                            question_name = 'landing-question.wav',
-                            next_page = 1,
-                            que_id = 0,
-                            ans_path = "static/audio/start-reading.wav",
-                            keys=['ok','yes','ready','好了','准备'],
-                            count_max=0)
+  return land(db, User, Question, 'p0.png', 'imgHDetails', page_conf_firetruck, 4, session, '快跑，云梯消防车！', 'firetruck', 'landing-question.wav', "start-reading.wav")
 
 @bp_ft.route('/process_answer', methods=['POST', 'GET'])
 def process_answer():
-  if request.method == "POST":
-    ans_data = request.get_json()
-    user = db.session.query(User).filter_by(username = session["username"]).first()
-    que_answered_before = user.que_answered
-
-    print(ans_data[0]['question'],ans_data[1]['result'])
-
-    # update que answered for the session user
-    if ans_data[0]['question'] > 0 and ans_data[1]['result']:
-      if que_answered_before:
-        user.que_answered += ';'+ str(ans_data[0]['question'])
-      else:
-        user.que_answered += str(ans_data[0]['question'])
-      db.session.commit()
-      print('db updated!')
-    
-    return 'ok'
+  return process(db, User, session)
 
 @bp_ft.route('/<int:page>')
 @login_required
 def innerbook(page):
-    idx = page - 1    
-    return render_template('firetruck/innerbook.html', 
-                        username =session["username"],
-                        img_name = page_conf_firetruck[idx]['img_name'],
-                        audio_name = page_conf_firetruck[idx]['audio_name'],
-                        question_name = page_conf_firetruck[idx]['que_audio'],
-                        next_page=page_conf_firetruck[idx]['next_page'],
-                        ans_path = page_conf_firetruck[idx]['ans_audio'],  
-                        keys=page_conf_firetruck[idx]['ans_keys'],
-                        count_max=page_conf_firetruck[idx]['count_max'],
-                        que_id = page_conf_firetruck[idx]['que_id'])
-
+  return inner(page, page_conf_firetruck, 'imgHDetails', '快跑，云梯消防车！', 'firetruck', session)
 
 @bp_baba.route('/landing')
 @login_required
 def landing():
-    uname = session["username"]
-    question_conf(page_conf_baba, uname, 3, 'static/audio/baba/')
-
-    return render_template('baba/landing.html', 
-                            username =session["username"],
-                            question_name = 'landing-question.wav',
-                            next_page = 1,
-                            que_id = 0,
-                            ans_path = "static/audio/landing-baba.wav",
-                            keys=['ok','yes','ready','好了','准备'],
-                            count_max=0)
+  return land(db, User, Question, 'barbabravo.gif', 'imgHDetails', page_conf_baba, 3, session, '巴巴布拉伯怕海藻', 'baba', 'landing-question.wav', "landing-baba.wav")
 
 @bp_baba.route('/process_answer', methods=['POST', 'GET'])
 def process_answer():
-  if request.method == "POST":
-    ans_data = request.get_json()
-    user = db.session.query(User).filter_by(username = session["username"]).first()
-    que_answered_before = user.que_answered
-
-    print(ans_data[0]['question'],ans_data[1]['result'])
-
-    # update que answered for the session user
-    if ans_data[0]['question'] > 0 and ans_data[1]['result']:
-      if que_answered_before:
-        user.que_answered += ';'+ str(ans_data[0]['question'])
-      else:
-        user.que_answered += str(ans_data[0]['question'])
-      db.session.commit()
-      print('db updated!')
-    
-    return 'ok'
+  return process(db, User, session)
 
 @bp_baba.route('/<int:page>')
 @login_required
 def innerbook(page):
-    idx = page - 1    
-    return render_template('baba/innerbook.html', 
-                        username =session["username"],
-                        img_name = page_conf_baba[idx]['img_name'],
-                        audio_name = page_conf_baba[idx]['audio_name'],
-                        question_name = page_conf_baba[idx]['que_audio'],
-                        next_page=page_conf_baba[idx]['next_page'],
-                        ans_path = page_conf_baba[idx]['ans_audio'],  
-                        keys=page_conf_baba[idx]['ans_keys'],
-                        count_max=page_conf_baba[idx]['count_max'],
-                        que_id = page_conf_baba[idx]['que_id'])
-
+    return inner(page, page_conf_baba, 'imgHDetails', '巴巴布拉伯怕海藻', 'baba', session)
 
 @bp_pigeon.route('/landing')
 @login_required
 def landing():
-    uname = session["username"]
-    question_conf(page_conf_pigeon, uname, 5, 'static/audio/pigeon/')
-
-    return render_template('pigeon/landing.html', 
-                            username =session["username"],
-                            question_name = 'landing-que-eng.wav',
-                            next_page = 1,
-                            que_id = 0,
-                            ans_path = "static/audio/start-reading-eng.wav",
-                            keys=['ok','yes','ready'],
-                            count_max=0)
+  return land(db, User, Question, 'p0.png', 'imgHDetails', page_conf_pigeon, 5, session, 'The Pigeon Has to Go to School', 'pigeon', 'landing-que-eng.wav', "start-reading-eng.wav")
 
 @bp_pigeon.route('/process_answer', methods=['POST', 'GET'])
 def process_answer():
-  if request.method == "POST":
-    ans_data = request.get_json()
-    user = db.session.query(User).filter_by(username = session["username"]).first()
-    que_answered_before = user.que_answered
-
-    print(ans_data[0]['question'],ans_data[1]['result'])
-
-    # update que answered for the session user
-    if ans_data[0]['question'] > 0 and ans_data[1]['result']:
-      if que_answered_before:
-        user.que_answered += ';'+ str(ans_data[0]['question'])
-      else:
-        user.que_answered += str(ans_data[0]['question'])
-      db.session.commit()
-      print('db updated!')
-    
-    return 'ok'
+  return process(db, User, session)
 
 @bp_pigeon.route('/<int:page>')
 @login_required
 def innerbook(page):
-    idx = page - 1    
-    return render_template('pigeon/innerbook.html', 
-                        username =session["username"],
-                        img_name = page_conf_pigeon[idx]['img_name'],
-                        audio_name = page_conf_pigeon[idx]['audio_name'],
-                        question_name = page_conf_pigeon[idx]['que_audio'],
-                        next_page=page_conf_pigeon[idx]['next_page'],
-                        ans_path = page_conf_pigeon[idx]['ans_audio'],  
-                        keys=page_conf_pigeon[idx]['ans_keys'],
-                        count_max=page_conf_pigeon[idx]['count_max'],
-                        que_id = page_conf_pigeon[idx]['que_id'])
-
-
-
+  return inner(page, page_conf_pigeon, 'imgLDetails', 'The Pigeon Has to Go to School', 'pigeon', session)
 
 @bp_nemo.route('/landing')
 @login_required
 def landing():
-    uname = session["username"]
-    question_conf(page_conf_nemo, uname, 2, 'static/audio/nemo/')
-
-    return render_template('nemo/landing.html', 
-                            username =session["username"],
-                            question_name = 'landing-que-eng.wav',
-                            next_page = 1,
-                            que_id = 0,
-                            ans_path = "static/audio/start-reading-eng.wav",
-                            keys=['ok','yes','ready'],
-                            count_max=0)
+    return land(db, User, Question, 'page0.png', 'imgHDetails', page_conf_nemo, 2, session, 'Finding Nemo', 'nemo', 'landing-que-eng.wav', "start-reading-eng.wav")
 
 @bp_nemo.route('/process_answer', methods=['POST', 'GET'])
 def process_answer():
-  if request.method == "POST":
-    ans_data = request.get_json()
-    user = db.session.query(User).filter_by(username = session["username"]).first()
-    que_answered_before = user.que_answered
-
-    print(ans_data[0]['question'],ans_data[1]['result'])
-
-    # update que answered for the session user
-    if ans_data[0]['question'] > 0 and ans_data[1]['result']:
-      if que_answered_before:
-        user.que_answered += ';'+ str(ans_data[0]['question'])
-      else:
-        user.que_answered += str(ans_data[0]['question'])
-      db.session.commit()
-      print('db updated!')
-    
-    return 'ok'
+  return process(db, User, session)
 
 @bp_nemo.route('/<int:page>')
 @login_required
 def innerbook(page):
-    idx = page - 1    
-    return render_template('nemo/innerbook.html', 
-                        username =session["username"],
-                        img_name = page_conf_nemo[idx]['img_name'],
-                        audio_name = page_conf_nemo[idx]['audio_name'],
-                        question_name = page_conf_nemo[idx]['que_audio'],
-                        next_page=page_conf_nemo[idx]['next_page'],
-                        ans_path = page_conf_nemo[idx]['ans_audio'],  
-                        keys=page_conf_nemo[idx]['ans_keys'],
-                        count_max=page_conf_nemo[idx]['count_max'],
-                        que_id = page_conf_nemo[idx]['que_id'])
-
+  return inner(page, page_conf_nemo, 'imgHDetails', 'Finding Nemo', 'nemo', session)
 
 @bp_100.route('/landing')
 @login_required
-def landing():
-    uname = session["username"]
-    question_conf(page_conf, uname, 1, 'static/audio/100/')
-
-    return render_template('100_floor/landing.html', 
-                            username =session["username"],
-                            question_name = 'landing-question.wav',
-                            next_page = 1,
-                            que_id = 0,
-                            ans_path = "static/audio/start-reading.wav",
-                            keys=['好了','准备'],
-                            count_max=0)
+def landing(): 
+  return land(db, User, Question, 'page0.png', 'imgDetails', page_conf, 1, session, '100层的房子', '100', 'landing-question.wav', "start-reading.wav")
 
 @bp_100.route('/process_answer', methods=['POST', 'GET'])
 def process_answer():
-  if request.method == "POST":
-    ans_data = request.get_json()
-    user = db.session.query(User).filter_by(username = session["username"]).first()
-    que_answered_before = user.que_answered
-
-    print(ans_data[0]['question'],ans_data[1]['result'])
-
-    # update que answered for the session user
-    if ans_data[0]['question'] > 0 and ans_data[1]['result']:
-      if que_answered_before:
-        user.que_answered += ';'+ str(ans_data[0]['question'])
-      else:
-        user.que_answered += str(ans_data[0]['question'])
-      db.session.commit()
-      print('db updated!')
-    
-    return 'ok'
-
+  return process(db, User, session)
 
 @bp_100.route('/<int:page>')
 @login_required
 def innerbook(page):
-    idx = page - 1    
-    return render_template('100_floor/innerbook.html', 
-                        username =session["username"],
-                        img_name = page_conf[idx]['img_name'],
-                        audio_name = page_conf[idx]['audio_name'],
-                        question_name = page_conf[idx]['que_audio'],
-                        next_page=page_conf[idx]['next_page'],
-                        ans_path = page_conf[idx]['ans_audio'],  
-                        keys=page_conf[idx]['ans_keys'],
-                        count_max=page_conf[idx]['count_max'],
-                        que_id = page_conf[idx]['que_id'])
+  return inner(page, page_conf, 'imgDetails', '100层的房子', '100', session)
+
+@bp_green.route('/landing')
+@login_required
+def landing(): 
+  return land(db, User, Question, 'p0.png', 'imgDetails', page_conf_worm, 7, session, 'The Gril With Green Eyes', 'green', 'landing-question.wav', "start-reading.wav")
+
+@bp_green.route('/process_answer', methods=['POST', 'GET'])
+def process_answer():
+  return process(db, User, session)
+
+@bp_green.route('/<int:page>')
+@login_required
+def innerbook(page):
+  return inner(page, page_conf_worm, 'imgSDetails', 'The Gril With Green Eyes', 'green', session)
+
+@bp_sally.route('/landing')
+@login_required
+def landing(): 
+  return land(db, User, Question, 'p0.png', 'imgDetails', page_conf_worm, 6, session, 'Sally\'s Phone', 'sally', 'landing-question.wav', "start-reading.wav")
+
+@bp_sally.route('/process_answer', methods=['POST', 'GET'])
+def process_answer():
+  return process(db, User, session)
+
+@bp_sally.route('/<int:page>')
+@login_required
+def innerbook(page):
+  return inner(page, page_conf_worm, 'imgSDetails', 'Sally\'s Phone', 'sally', session)
+
 
 class Main(views.MethodView):
     def get(self):
@@ -399,37 +180,12 @@ class Main(views.MethodView):
             flash("Username doesn't exist or incorrect password")
         return redirect(url_for('index'))
 
-# #obseleted code starts
-# nemo_1_keys = ['Orange', 'Stripe', 'Black', 'White', 'orange', 'white']
-# nemo_2_keys = ['Forget', 'Remember', 'Memory', 'loss', 'Issue', 'forget', 'remember']
-
-# bp_nemo_1 = Blueprint('nemo_1', __name__, static_folder='static',
-#                template_folder='templates')
-# bp_nemo_2 = Blueprint('nemo_2', __name__, static_folder='static',
-#                template_folder='templates')
-
-
-# @bp_nemo_1.route('/')
-# def index():
-#     return render_template('nemo_1/index.html', 
-#                            ans_path = "static/audio/prompt_answer.wav", 
-#                            username =session["username"],
-#                            keys=nemo_1_keys) 
-
-# @bp_nemo_2.route('/')
-# def index():
-#     return render_template('nemo_2/index.html', 
-#                            ans_path = "static/audio/prompt-dory-anw.wav", 
-#                            username =session["username"], 
-#                            keys=nemo_2_keys)
-## obseleted code ends
-
 app.add_url_rule('/',
                  view_func=Main.as_view('index'),
                  methods=["GET", "POST"])
 
-# app.register_blueprint(bp_nemo_1, url_prefix='/nemo_1')
-# app.register_blueprint(bp_nemo_2, url_prefix='/nemo_2')
+app.register_blueprint(bp_green, url_prefix='/green')
+app.register_blueprint(bp_sally, url_prefix='/sally')
 app.register_blueprint(bp_100, url_prefix='/100_floor')
 app.register_blueprint(bp_nemo, url_prefix='/nemo')
 app.register_blueprint(bp_pigeon, url_prefix='/pigeon')
